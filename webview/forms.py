f77fc65fa523e1d4ad25316a940984982d2a1c1a
mr-erold <mmpterold@gmail.com>
2022-07-11 23:47:59 +0200

    Define a generic Item information flask form

---
+++ webview/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, FloatField, IntegerField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class ItemInformationForm(FlaskForm):
+    item_name = StringField('Item Name', validators=[DataRequired()])
+    item_price = FloatField('Item Price', validators=[DataRequired()])
+    item_quantity = IntegerField('Item quantity', validators=[DataRequired()])
+    save_btn = SubmitField('Save')
