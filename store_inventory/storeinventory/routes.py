86ac4f126ab7e629f55b2f7d0961614f22cef2f9
mr-erold <mmpterold@gmail.com>
2022-07-13 06:58:14 +0200

    Add support for updating item information

--- store_inventory/storeinventory/routes.py
+++ store_inventory/storeinventory/routes.py
@@ -7,54 +7,6 @@ import requests
 from storeinventory import app
 from storeinventory.forms import ItemInformationForm
 
-items = [
-    {
-        'name': "Apples",
-        'price': 31.00,
-        'quantity': 34
-    },
-    {
-        'name': "Bananas",
-        'price': 27.65,
-        'quantity': 23
-    },
-    {
-        'name': "Blueberries",
-        'price': 35.00,
-        'quantity': 20
-    },
-    {
-        'name': "Cinamon",
-        'price': 22.50,
-        'quantity': 25
-    },
-    {
-        'name': "Honey",
-        'price': 26.89,
-        'quantity': 12
-    },
-    {
-        'name': "Onions",
-        'price': 7.5,
-        'quantity': 78
-    },
-    {
-        'name': "Oranges",
-        'price': 29.99,
-        'quantity': 123
-    },
-    {
-        'name': "Pepper",
-        'price': 13.00,
-        'quantity': 12
-    },
-    {
-        'name': "Potatoes",
-        'price': 40.00,
-        'quantity': 34
-    }
-]
-
 
 @app.route("/", methods=['GET'])
 def home():
